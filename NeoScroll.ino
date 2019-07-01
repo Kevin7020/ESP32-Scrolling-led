f92b82a42ba8d40285cba8b503a640e90f0177ee
Kevin Porras R <kevin702010@hotmail.com>
2019-06-30 21:35:51 -0600

    Added gitignore. ledPxLength is going to let us know how much do we have to scroll the text.

--- NeoScroll.ino
+++ NeoScroll.ino
@@ -4,55 +4,64 @@
 
 #define PIN 18
 
-// MATRIX DECLARATION:
-// Parameter 1 = width of NeoPixel matrix
-// Parameter 2 = height of matrix
-// Parameter 3 = pin number (most are valid)
-// Parameter 4 = matrix layout flags, add together as needed:
-//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
-//     Position of the FIRST LED in the matrix; pick two, e.g.
-//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
-//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs are arranged in horizontal
-//     rows or in vertical columns, respectively; pick one or the other.
-//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns proceed
-//     in the same order, or alternate lines reverse direction; pick one.
-//   See example below for these values in action.
-// Parameter 5 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
-  NEO_MATRIX_BOTTOM    + NEO_MATRIX_RIGHT +
+  NEO_MATRIX_BOTTOM  + NEO_MATRIX_RIGHT +
   NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
   NEO_GRB            + NEO_KHZ800);
 
 const uint16_t colors[] = {
-  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(255, 255, 0),matrix.Color(0, 0, 255), matrix.Color(255, 0, 255), matrix.Color(0, 255, 255), matrix.Color(255, 255, 255)};
+    matrix.Color(255, 0, 0),
+    matrix.Color(0, 255, 0),
+    matrix.Color(255, 255, 0),
+    matrix.Color(0, 0, 255),
+    matrix.Color(255, 0, 255),
+    matrix.Color(0, 255, 255),
+    matrix.Color(255, 255, 255)};
 
-void setup() {
+String texto = "HELLO WORLD!";
+
+int ledPxLength(int strLength)
+{
+  /*"HELLO WORLD!" Contains 12 caracters and each caracter has a lenght of 5px with 7px of height
+   *Each caracter has 1px of padding to separate it from the other.
+   *https://cdn-learn.adafruit.com/downloads/pdf/adafruit-gfx-graphics-library.pdf
+   *
+   *In order to display "Hello world!" we need 60px + 12px of padding on the X axis of the screen.
+   */
+  //Serial.println(strLength);
+  int lengthBuffer = strLength;
+  strLength = (((-1 * strLength)*5)+(-1 * lengthBuffer));
+  //Serial.println(strLength);
+  return strLength;
+}
+
+void setup()
+{
+  Serial.begin(115200);
   matrix.begin();
   matrix.setTextWrap(false);
   matrix.setBrightness(5);
   matrix.setTextColor(colors[0]);
 }
 
-int x    = matrix.width();
+int x = matrix.width();
 int pass = 0;
 
-void loop() {
+void loop()
+{
   matrix.fillScreen(0);
   matrix.setCursor(x, 0);
-  matrix.print(F("Test"));
+  matrix.print(texto);
 
-  if(--x < -30
-  ) {
+  Serial.printf("FreeMem:%d %d \n", ESP.getFreeHeap(), x);
+  if ( --x < ledPxLength(texto.length()) )
+  {
     x = matrix.width();
 
-    if(++pass >= 8) pass = 0;
+    if (++pass >= 8)
+      pass = 0;
     matrix.setTextColor(colors[pass]);
   }
   matrix.show();
-  delay(30);
+  delay(35);
 }
