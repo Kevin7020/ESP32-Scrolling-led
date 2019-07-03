8b96ee2c5e5e98e34d3f8a6556970b7377cdddfa
Kevin Porras R <kevin702010@hotmail.com>
2019-07-02 22:46:56 -0600

    Implemented initial webserver

--- NeoScroll.ino
+++ NeoScroll.ino
@@ -4,6 +4,28 @@
 #include "ledBanner.h"
 
 String texto = "HELLO WORLD!";
+const char *ssid = "YourSSIDHere";
+const char *password = "YourPSKHere";
+WebServer server(80);
+
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
 
 
 
@@ -11,9 +33,33 @@ void setup()
 {
   Serial.begin(115200);
   ledInit();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("")
+
+      while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  if (MDNS.begin("esp32"))
+  {
+    Serial.println("MDNS responder started");
+  }
+  //Server routes
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop()
 {
-ledBanner(texto);
+  ledBanner(texto);
+  server.handleClient();
 }
